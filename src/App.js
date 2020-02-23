76af38c63e4059ccd0d13fff09061893e3115295
Sergey <sm.dnd@ya.ru>
2020-02-23 17:48:00 +0300

    commit 2

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import Header from "./Header";
+import Technologies from "./Header";
+
+const  App = () => {
+  return (
+      <div>
+          {/*<Header/>*/}
+          <Technologies/>
+
+          </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const   Header = () =>{
+    return (<div>
+        <a href="#">Home</a>
+        <a href="#">New Feed</a>
+        <a href="#">Messages</a>
+    </div>);
+}
+export default Header;
